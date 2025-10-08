39625f48551567e90e55a9407d8a15e5ff426680
nnatttaly <dmitrievan730@gmail.com>
2025-10-09 03:42:15 +0500

    Создан компонент для страницы «404»

---
+++ src/pages/not-found-pages/not-found-page.tsx
@@ -0,0 +1,18 @@
+function NotFoundPage(): JSX.Element {
+  return (
+    <div>
+      <div>
+        <h1>404 Not Found</h1>
+        <p>
+          Кажется, кто-то съел эту страницу
+        </p>
+        <a href="/">
+          Вернуться на главную
+        </a>
+      </div>
+    </div>
+  );
+
+}
+
+export default NotFoundPage;
